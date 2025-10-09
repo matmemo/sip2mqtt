da27b8e2b2abe63934625437326261cf52d716e9
mat <matmemo@noreply.github.com>
2025-10-09 19:05:06 +0200

    separate code into MqttClient and SipClient class

--- sip2mqtt.py
+++ sip2mqtt.py
@@ -11,13 +11,6 @@ import sys
 from dotenv import load_dotenv
 from pyVoIP.VoIP import VoIPPhone, InvalidStateError
 
-run_flag = True
-mqtt_online_flag = False
-sip_online_flag = False
-
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-mqtt_unacked_publish = set()
-
 class Config:
     def __init__(self):
         self.MQTT_HOST = self.check_env("MQTT_HOST")
@@ -52,118 +45,142 @@ class Config:
                 f"SIP_REGISTRAR_PORT: {self.SIP_REGISTRAR_PORT}\n" \
                 f"SIP_USER: {self.SIP_USER}\n" \
                 f"SIP_PASS: {"*" * len(self.SIP_PASS) * 2}\n" \
-                f"SIP_CLIENT_ADDR: {self.SIP_CLIENT_ADDR}\n"
+                f"SIP_CLIENT_ADDR: {self.SIP_CLIENT_ADDR}\n" \
                 f"SIP_CLIENT_PORT: {self.SIP_CLIENT_PORT}"
         )
 
-# mqtt client callbacks
-def mqtt_on_connect(client, userdata, flags, reason_code, properties):
-    global mqtt_online_flag
-    logging.info(f"mqtt broker connection established with result code: {reason_code}")
-    mqtt_online_flag = True
+class MqttClient:
+    def __init__(self):
+        self.online = False
+        self.connection_failed = False
+        self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.unacked_publish = set()
 
-def mqtt_on_connect_fail(client, userdata):
-    global run_flag
-    logging.error("Connection to mqtt broker failed. Exiting")
-    run_flag = False
+    def _on_connect(self, client, userdata, flags, reason_code, properties):
+        logging.info(f"mqtt broker connection established with result code: {reason_code}")
+        self.online = True
 
-def mqtt_on_disconnect(client, userdata, disconnect_flags, reason_code, properties):
-    global mqtt_online_flag
-    logging.info("Disconnected from mqtt broker")
-    mqtt_online_flag = False
+    def _on_connect_fail(self, client, userdata):
+        logging.error("Connection to mqtt broker failed. Exiting")
+        self.connection_failed = True
 
-def mqtt_on_publish(client, userdata, mid, reason_code, properties):
-    try:
-        userdata.remove(mid)
-    except KeyError:
-        print("on_publish() is called with a mid not present in unacked_publish")
+    def _on_disconnect(self, client, userdata, disconnect_flags, reason_code, properties):
+        logging.info("Disconnected from mqtt broker")
+        self.online = False
 
-def mqtt_init(config):
-    global mqttc, mqtt_unacked_publish
-    mqttc.on_connect = mqtt_on_connect
-    mqttc.on_connect_fail = mqtt_on_connect_fail
-    mqttc.on_disconnect = mqtt_on_disconnect
-    mqttc.on_publish = mqtt_on_publish
+    def _on_publish(self, client, userdata, mid, reason_code, properties):
+        try:
+            userdata.remove(mid)
+        except KeyError:
+            logging.error("on_publish() is called with a mid not present in unacked_publish")
 
-    mqttc.user_data_set(mqtt_unacked_publish)
+    def init(self, config):
+        self.mqttc.on_connect = self._on_connect
+        self.mqttc.on_connect_fail = self._on_connect_fail
+        self.mqttc.on_disconnect = self._on_disconnect
+        self.mqttc.on_publish = self._on_publish
 
-    logging.info(f"Trying to connect to mqtt broker {config.MQTT_HOST}:{config.MQTT_PORT}")
-    mqttc.will_set(config.MQTT_BASETOPIC + "/status", "offline", qos=0, retain=True)
-    mqttc.connect(config.MQTT_HOST, config.MQTT_PORT, 60)
-    mqttc.loop_start()
+        self.mqttc.user_data_set(self.unacked_publish)
 
-    mqtt_publish_status(config, "online")
+        return self.mqttc
 
-    return mqttc
+    def start(self, config):
+        logging.info(f"Trying to connect to mqtt broker {config.MQTT_HOST}:{config.MQTT_PORT}")
+        self.mqttc.will_set(config.MQTT_BASETOPIC + "/status", "offline", qos=0, retain=True)
+        self.mqttc.connect(config.MQTT_HOST, config.MQTT_PORT, 60)
+        self.mqttc.loop_start()
 
-def mqtt_publish(topic, message):
-    global mqttc, mqtt_unacked_publish
-    msg_info = mqttc.publish(config.MQTT_BASETOPIC, message, qos=1)
-    mqtt_unacked_publish.add(msg_info.mid)
+        self.publish_status(config, "online")
+        self.online = True
 
-    while len(mqtt_unacked_publish):
-        time.sleep(0.1)
+        return self.mqttc
 
-    msg_info.wait_for_publish()
+    def stop(self):
+        self.publish_status(config, "offline")
+        self.mqttc.disconnect()
+        logging.info("mqtt client stopped")
 
-def mqtt_publish_retained(topic, message):
-    global mqttc, mqtt_unacked_publish
-    msg_info = mqttc.publish(config.MQTT_BASETOPIC, message, qos=1, retain=True)
-    mqtt_unacked_publish.add(msg_info.mid)
+    def publish(self, topic, message):
+        msg_info = self.mqttc.publish(topic, message, qos=1)
+        self.unacked_publish.add(msg_info.mid)
 
-    while len(mqtt_unacked_publish):
-        time.sleep(0.1)
+        while len(self.unacked_publish):
+            time.sleep(0.1)
 
-    msg_info.wait_for_publish()
+        msg_info.wait_for_publish()
 
-def mqtt_publish_status(config, status):
-    mqtt_publish_retained(config.MQTT_BASETOPIC + "/status", status)
+    def publish_retained(self, topic, message):
+        msg_info = self.mqttc.publish(topic, message, qos=1, retain=True)
+        self.unacked_publish.add(msg_info.mid)
 
-# sip client callbacks
-def sip_handle_call(call):
-    try:
-        sip_message = call.request
-        mqtt_message = {
-            "type": "request",
-            "method": sip_message.method,
-            "headers": sip_message.headers
-        }
+        while len(self.unacked_publish):
+            time.sleep(0.1)
 
-        # print(f"Method: {sip_message.method}")
-        # print(f"Headers: {json.dumps(sip_message.headers, indent=4)}")
+        msg_info.wait_for_publish()
 
-        mqtt_publish(config.MQTT_BASETOPIC + "/event", json.dumps(mqtt_message))
+    def publish_status(self, config, status):
+        self.publish_retained(config.MQTT_BASETOPIC + "/status", status)
 
-        time.sleep(0.5)
-        call.deny()
-    except InvalidStateError:
-        pass
-  
-def sip_init(config):
-    sip_client = VoIPPhone(
-            server = config.SIP_REGISTRAR_HOST,
-            port = config.SIP_REGISTRAR_PORT,
-            username = config.SIP_USER,
-            password = config.SIP_PASS,
-            myIP = config.SIP_CLIENT_ADDR,
-            callCallback = sip_handle_call,
-            sipPort = config.SIP_CLIENT_PORT
-    )
 
-    sip_client.start()
+class SipClient:
+    def __init__(self, publisher):
+        self.online = False
+        self.publisher = publisher
 
-    return sip_client
+    def _handle_call(self, call):
+        try:
+            sip_message = call.request
+            mqtt_message = {
+                "type": "request",
+                "method": sip_message.method,
+                "headers": sip_message.headers
+            }
+
+            logging.info(f"sip {sip_message.method} received")
+            # logging.debug(json.dumps(mqtt_message, indent=2)}")
+            self.publisher(json.dumps(mqtt_message))
+
+            time.sleep(0.5)
+            call.deny()
+        except InvalidStateError:
+            pass
+      
+    def init(self, config):
+        self.client = VoIPPhone(
+                server = config.SIP_REGISTRAR_HOST,
+                port = config.SIP_REGISTRAR_PORT,
+                username = config.SIP_USER,
+                password = config.SIP_PASS,
+                myIP = config.SIP_CLIENT_ADDR,
+                callCallback = self._handle_call,
+                sipPort = config.SIP_CLIENT_PORT
+        )
+
+        return self.client
+
+    def start(self, config):
+        self.client.start()
+        self.online = True
+        logging.info("sip client started")
+
+    def stop(self):
+        self.client.stop()
+        self.online = False
+        logging.info("sip client stopped")
 
 if __name__ == "__main__":
+    # load config
     load_dotenv()
     config = Config()
 
+    # setup logging
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%Y-%m-%d %H:%M:%S")
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 
-    logging.info("sip2mqtt started. Config: \n" + str(config))
+    logging.info("sip2mqtt initializing. Config: \n" + str(config))
 
+    # setup signal handlers
+    run_flag = True
     def stop_signals_handler(_signo, _stack_frame):
         global run_flag
         logging.info("Received termination event")
@@ -172,23 +189,35 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, stop_signals_handler)
     signal.signal(signal.SIGTERM, stop_signals_handler)
 
-    mqttc = mqtt_init(config)
-    while run_flag and not mqtt_online_flag:
+    # setup mqtt client
+    mqttc = MqttClient()
+
+    # define publisher callback for SipClient
+    def publisher(message):
+        mqttc.publish(config.MQTT_BASETOPIC + "/event", message)
+
+    # setup sip client
+    sipc = SipClient(publisher)
+
+    # start mqtt client
+    mqttc.init(config)
+    mqttc.start(config)
+    while run_flag and not mqttc.online:
         time.sleep(0.1)
 
-    sip_client = sip_init(config)
-    logging.info("sip client started")
-    logging.info("listening")
+    # start sip client
+    sipc.init(config)
+    sipc.start(config)
+    while run_flag and not sipc.online:
+        time.sleep(0.1)
 
+    # loop until stopped
+    logging.info("listening...")
     while run_flag:
-        time.sleep(0.1)
+        time.sleep(0.5)
 
-    sip_client.stop()
-    logging.info("sip client stopped")
-
-    mqtt_publish_status(config, "offline")
-    mqttc.disconnect()
-    mqttc.loop_stop()
-    logging.info("mqtt client stopped")
+    # wind down
+    sipc.stop()
+    mqttc.stop()
 
     logging.info("Exiting")
